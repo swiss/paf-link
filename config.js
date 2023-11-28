6e91ebac1b54f020296d6ee867b54fa0b1e35d0f
Benedikt Hitz-Gamper <bhitz@gmx.net>
2023-11-28 17:23:31 +0100

    Create config.js

---
+++ config.js
@@ -0,0 +1,32 @@
+var respecConfig = {
+    specStatus: "unofficial",
+    shortName: 'paf-link',
+    edDraftURI: "https://paf.link/",
+    latestVersion: null,
+    maxTocLevel: 4,
+    postProcess: [jumpTo],
+    github: {
+      repoURL: "https://github.com/bequrios/paf-link",
+      branch: "main", // alternative branch
+    },
+    editors: [{
+      name: "Théophile Boucard",
+      company: "Federal Chancellery (FCh)",
+      companyURL: "https://www.bk.admin.ch",
+      url: "mailto:theophile.boucard@bk.admin.ch"
+    },
+    {
+      name: "Michael Luggen",
+      company: "Federal Chancellery (FCh)",
+      companyURL: "https://www.bk.admin.ch",
+      url: "mailto:michael.luggen@bk.admin.ch"
+    },
+    {
+      name: "Benedikt Hitz-Gamper",
+      company: "Bern University of Applied Sciences (BFH)",
+      companyURL: "https://www.bfh.ch",
+      url: "mailto:benedikt.hitz@bfh.ch",
+    }]
+  };
+
+module.exports = respecConfig
