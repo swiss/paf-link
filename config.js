df0037d2ec1f7ab7de2de2e6238506704a08b70f
Michael Luggen <github.michael@oiu.ch>
2025-03-14 18:05:50 +0100

    update editors

--- config.js
+++ config.js
@@ -11,35 +11,33 @@ var respecConfig = {
       repoURL: "https://github.com/swiss/paf-link",
       branch: "main", // alternative branch
     },
-    editors: [{
+    formerEditors: [{
       name: "Jörg De Bernardi",
       company: "Federal Chancellery (FCh)",
       companyURL: "https://www.bk.admin.ch",
       url: "mailto:joerg.debernardi@bk.admin.ch"
     },
     {
-      name: "Louis Bernath",
-      company: "Federal Chancellery (FCh)",
-      companyURL: "https://www.bk.admin.ch",
-      url: "mailto:louis.bernath@bk.admin.ch"
-    },
-    {
       name: "Théophile Boucard",
       company: "Federal Chancellery (FCh)",
       companyURL: "https://www.bk.admin.ch",
       url: "mailto:theophile.boucard@bk.admin.ch"
-    },
-    {
-      name: "Michael Luggen",
-      company: "Federal Chancellery (FCh)",
-      companyURL: "https://www.bk.admin.ch",
-      url: "mailto:michael.luggen@bk.admin.ch"
-    },
-    {
+    }],
+    editors: [{
       name: "Benedikt Hitz-Gamper",
       company: "Bern University of Applied Sciences (BFH)",
       companyURL: "https://www.bfh.ch",
       url: "mailto:benedikt.hitz@bfh.ch",
+    },{
+      name: "Michael Luggen",
+      company: "Federal Chancellery (FCh)",
+      companyURL: "https://www.bk.admin.ch",
+      url: "mailto:michael.luggen@bk.admin.ch"
+    },{
+      name: "Louis Bernath",
+      company: "Federal Chancellery (FCh)",
+      companyURL: "https://www.bk.admin.ch",
+      url: "mailto:louis.bernath@bk.admin.ch"
     }],
     logos: [
       {
